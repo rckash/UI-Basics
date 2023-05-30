791880faea8f8dffc76d613df0b847c349b65b4c
RC <rckashyapcpe@gmail.com>
2023-05-30 19:17:20 +0800

    added another activity

--- app/src/main/java/com/example/uibasics/MainActivity2.kt
+++ app/src/main/java/com/example/uibasics/MainActivity2.kt
@@ -2,10 +2,20 @@ package com.example.uibasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var button : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        //findViewbyId
+        button = findViewById(R.id.buttonProfile)
+
+        button.setOnClickListener{
+            Toast.makeText(applicationContext,"Profile now can be edited",Toast.LENGTH_LONG).show()
+        }
     }
 }
